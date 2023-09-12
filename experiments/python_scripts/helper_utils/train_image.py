385ea8fd7d53313aa85689f9d13dd2ca5da89c59
NOBUYUK1 <122588734+NOBUYUK1@users.noreply.github.com>
2023-09-12 11:10:35 +0000

    updated example code

--- experiments/python_scripts/helper_utils/train_image.py
+++ experiments/python_scripts/helper_utils/train_image.py
@@ -151,4 +151,127 @@ def train(config):
             if temp_acc > best_acc:
                 best_acc = temp_acc
                 best_model = temp_model
-            log_str = "iter: {:05d}, precision: {:.5f}".format(i
+            log_str = "iter: {:05d}, precision: {:.5f}".format(i, temp_acc)
+            config["out_file"].write(log_str+"\n")
+            config["out_file"].flush()
+            print(log_str)
+        if i % config["snapshot_interval"] == 0:
+            torch.save(nn.Sequential(base_network), osp.join(config["output_path"], \
+                "iter_{:05d}_model.pth.tar".format(i)))
+
+        loss_params = config["loss"]                  
+        ## train one iter
+        base_network.train(True)
+        ad_net.train(True)
+        optimizer = lr_scheduler(optimizer, i, **schedule_param)
+        optimizer.zero_grad()
+        if i % len_train_source == 0:
+            iter_source = iter(dset_loaders["source"])
+        if i % len_train_target == 0:
+            iter_target = iter(dset_loaders["target"])
+        inputs_source, labels_source = iter_source.next()
+        inputs_target, labels_target = iter_target.next()
+        inputs_source, inputs_target, labels_source = inputs_source.cuda(), inputs_target.cuda(), labels_source.cuda()
+        features_source, outputs_source = base_network(inputs_source)
+        features_target, outputs_target = base_network(inputs_target)
+        features = torch.cat((features_source, features_target), dim=0)
+        outputs = torch.cat((outputs_source, outputs_target), dim=0)
+        softmax_out = nn.Softmax(dim=1)(outputs)
+        if config['method'] == 'CDAN+E':           
+            entropy = loss.Entropy(softmax_out)
+            transfer_loss = loss.CDAN([features, softmax_out], ad_net, entropy, network.calc_coeff(i), random_layer)
+        elif config['method']  == 'CDAN':
+            transfer_loss = loss.CDAN([features, softmax_out], ad_net, None, None, random_layer)
+        elif config['method']  == 'DANN':
+            transfer_loss = loss.DANN(features, ad_net)
+        else:
+            raise ValueError('Method cannot be recognized.')
+        classifier_loss = nn.CrossEntropyLoss()(outputs_source, labels_source)
+        total_loss = loss_params["trade_off"] * transfer_loss + classifier_loss
+        total_loss.backward()
+        optimizer.step()
+    torch.save(best_model, osp.join(config["output_path"], "best_model.pth.tar"))
+    return best_acc
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Conditional Domain Adversarial Network')
+    parser.add_argument('method', type=str, default='CDAN+E', choices=['CDAN', 'CDAN+E', 'DANN'])
+    parser.add_argument('--gpu_id', type=str, nargs='?', default='0', help="device id to run")
+    parser.add_argument('--net', type=str, default='ResNet50', choices=["ResNet18", "ResNet34", "ResNet50", "ResNet101", "ResNet152", "VGG11", "VGG13", "VGG16", "VGG19", "VGG11BN", "VGG13BN", "VGG16BN", "VGG19BN", "AlexNet"])
+    parser.add_argument('--dset', type=str, default='office', choices=['office', 'image-clef', 'visda', 'office-home'], help="The dataset or source dataset used")
+    parser.add_argument('--s_dset_path', type=str, default='../../data/office/amazon_31_list.txt', help="The source dataset path list")
+    parser.add_argument('--t_dset_path', type=str, default='../../data/office/webcam_10_list.txt', help="The target dataset path list")
+    parser.add_argument('--test_interval', type=int, default=500, help="interval of two continuous test phase")
+    parser.add_argument('--snapshot_interval', type=int, default=5000, help="interval of two continuous output model")
+    parser.add_argument('--output_dir', type=str, default='san', help="output directory of our model (in ../snapshot directory)")
+    parser.add_argument('--lr', type=float, default=0.001, help="learning rate")
+    parser.add_argument('--random', type=bool, default=False, help="whether use random projection")
+    args = parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    #os.environ["CUDA_VISIBLE_DEVICES"] = '0,1,2,3'
+
+    # train config
+    config = {}
+    config['method'] = args.method
+    config["gpu"] = args.gpu_id
+    config["num_iterations"] = 100004
+    config["test_interval"] = args.test_interval
+    config["snapshot_interval"] = args.snapshot_interval
+    config["output_for_test"] = True
+    config["output_path"] = "snapshot/" + args.output_dir
+    if not osp.exists(config["output_path"]):
+        os.system('mkdir -p '+config["output_path"])
+    config["out_file"] = open(osp.join(config["output_path"], "log.txt"), "w")
+    if not osp.exists(config["output_path"]):
+        os.mkdir(config["output_path"])
+
+    config["prep"] = {"test_10crop":True, 'params':{"resize_size":256, "crop_size":224, 'alexnet':False}}
+    config["loss"] = {"trade_off":1.0}
+    if "AlexNet" in args.net:
+        config["prep"]['params']['alexnet'] = True
+        config["prep"]['params']['crop_size'] = 227
+        config["network"] = {"name":network.AlexNetFc, \
+            "params":{"use_bottleneck":True, "bottleneck_dim":256, "new_cls":True} }
+    elif "ResNet" in args.net:
+        config["network"] = {"name":network.ResNetFc, \
+            "params":{"resnet_name":args.net, "use_bottleneck":True, "bottleneck_dim":256, "new_cls":True} }
+    elif "VGG" in args.net:
+        config["network"] = {"name":network.VGGFc, \
+            "params":{"vgg_name":args.net, "use_bottleneck":True, "bottleneck_dim":256, "new_cls":True} }
+    config["loss"]["random"] = args.random
+    config["loss"]["random_dim"] = 1024
+
+    config["optimizer"] = {"type":optim.SGD, "optim_params":{'lr':args.lr, "momentum":0.9, \
+                           "weight_decay":0.0005, "nesterov":True}, "lr_type":"inv", \
+                           "lr_param":{"lr":args.lr, "gamma":0.001, "power":0.75} }
+
+    config["dataset"] = args.dset
+    config["data"] = {"source":{"list_path":args.s_dset_path, "batch_size":36}, \
+                      "target":{"list_path":args.t_dset_path, "batch_size":36}, \
+                      "test":{"list_path":args.t_dset_path, "batch_size":4}}
+
+    if config["dataset"] == "office":
+        if ("amazon" in args.s_dset_path and "webcam" in args.t_dset_path) or \
+           ("webcam" in args.s_dset_path and "dslr" in args.t_dset_path) or \
+           ("webcam" in args.s_dset_path and "amazon" in args.t_dset_path) or \
+           ("dslr" in args.s_dset_path and "amazon" in args.t_dset_path):
+            config["optimizer"]["lr_param"]["lr"] = 0.001 # optimal parameters
+        elif ("amazon" in args.s_dset_path and "dslr" in args.t_dset_path) or \
+             ("dslr" in args.s_dset_path and "webcam" in args.t_dset_path):
+            config["optimizer"]["lr_param"]["lr"] = 0.0003 # optimal parameters       
+        config["network"]["params"]["class_num"] = 31 
+    elif config["dataset"] == "image-clef":
+        config["optimizer"]["lr_param"]["lr"] = 0.001 # optimal parameters
+        config["network"]["params"]["class_num"] = 12
+    elif config["dataset"] == "visda":
+        config["optimizer"]["lr_param"]["lr"] = 0.001 # optimal parameters
+        config["network"]["params"]["class_num"] = 12
+        config['loss']["trade_off"] = 1.0
+    elif config["dataset"] == "office-home":
+        config["optimizer"]["lr_param"]["lr"] = 0.001 # optimal parameters
+        config["network"]["params"]["class_num"] = 65
+    else:
+        raise ValueError('Dataset cannot be recognized. Please define your own dataset here.')
+    config["out_file"].write(str(config))
+    config["out_file"].flush()
+    train(config)
